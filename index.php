60dd9a6c8747f432ff28e1b2612c2e4e41ed8771
I481695 <mette.ghijsen@student.fontys.nl>
2021-11-01 12:50:33 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Todo-List</title>
+</head>
+<body>
+    <label class="titel">Mette's Todo-list</label><br>
+
+    <div class="alles">
+    <div class="textboxenknop">
+    <input type="text" id="textbox" class="textbox"><br>
+    <input onclick="AddItem()" type="submit" id="button" class="btn btn-primary" value="Add item">
+    </div>
+        <br>
+    <ul id="todolist" class="todolist">
+        <li class="checked">Portfolio Website aanpassen</li>
+        <li>Todo-list programmeren</li>
+    </ul>
+    </div>
+
+</body>
+<script src="script.js"></script>
+</html>
---
+++ script.js
@@ -0,0 +1,23 @@
+let todoItem = document.getElementById('textbox');
+let todolist = document.getElementById('todolist');
+
+class Item {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+let todoItems = [];
+
+function AddItem() {
+    let content = todoItem.value;
+    let newItem = new Item(content);
+    todoItems.push(newItem);
+
+    const newLi = document.createElement("li");
+    const newContent = document.createTextNode(content);
+    newLi.appendChild(newContent);
+    todolist.append(newLi);
+}
+
+
